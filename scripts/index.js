fbb8d610e93076141db439d10fc2546b533eba2a
Eugene Salautin <beefdoshirak@yandex.ru>
2023-03-24 15:56:10 +0700

    feat: добавить скрипты для модального окна

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const editBtn = document.querySelector('.profile__edit-btn');
+const editPopup = document.querySelector('.popup');
+const editPopupCloseBtn = editPopup.querySelector('.popup__close-btn');
+const editPopupForm = editPopup.querySelector('.popup__form');
+const nameInput = editPopup.querySelector('.popup__text_name');
+const statusInput = editPopup.querySelector('.popup__text_status');
+const profileName = document.querySelector('.profile__title');
+const profileStatus = document.querySelector('.profile__subtitle');
+
+
+editBtn.addEventListener('click', () => {
+    editPopup.classList.add('popup_open');
+    nameInput.value = profileName.textContent;
+    statusInput.value = profileStatus.textContent;
+});
+editPopupCloseBtn.addEventListener('click', () => {
+    editPopup.classList.remove('popup_open');
+});
+
+
+editPopupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = nameInput.value;
+    const status = statusInput.value;
+
+    profileName.textContent = name;
+    profileStatus.textContent = status;
+    editPopup.classList.remove('popup_open');
+});
